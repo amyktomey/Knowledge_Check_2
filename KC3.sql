242b688254a721e3fa42d5b819f4b7b3dfa0e4c1
Amy Tomey <amy.tomey59@gmail.com>
2023-10-29 23:27:08 -0400

    added second query

--- KC3.sql
+++ KC3.sql
@@ -2,3 +2,8 @@ SELECT Orders.OrderID, Customers.CustomerName
 FROM Orders
 INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID
 where OrderID= 10310;
+
+SELECT Products.SupplierID, Suppliers.Address
+FROM Products
+INNER JOIN Suppliers ON Products.SupplierID=Suppliers.SupplierID
+where ProductID = 40;
